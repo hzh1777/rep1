7341f7d12f03ece3f2be2c72786b0172ef705b45
zhangsan <zhang@itcast.cn>
2019-11-02 11:16:31 +0800

    第一次提交demo.java

--- git/src/com/itheima/test/Demo.java
+++ git/src/com/itheima/test/Demo.java
@@ -1,7 +1,8 @@
 package com.itheima.test;
 
-public class   Demo {
+public class Demo {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("我是老鸟");
     }
 }
