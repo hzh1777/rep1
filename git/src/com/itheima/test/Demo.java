d21ba51896a9b7924032054fdb975e1b043dfc1e
zhangsan <zhang@itcast.cn>
2019-11-02 11:26:39 +0800

    添加回复

--- git/src/com/itheima/test/Demo.java
+++ git/src/com/itheima/test/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("我是老鸟");
+        System.out.println("你好傻");
     }
 }
